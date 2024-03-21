52cf3d0be13660dd544e7ada0343bb2fb5798588
rennehuattamamani <rennehuatta@gmail.com>
2024-03-20 23:20:35 -0500

    Actualizacion

---
+++ class07.js
@@ -0,0 +1,24 @@
+function producto(){
+    let num1=3;
+    let num2=7;
+    producto=num1*num2;
+    return producto;
+}
+
+resultado=producto();
+console.log(resultado);
+
+document.write ('Hey ¿Qué tal?');
+userName=prompt('¿Cómo te llamas?:');
+document.write('Bienvenido'+ ' ' + userName);
+
+userempresa=prompt ('¿Qué estas buscado?:');
+ocument.write('Comprendido' + ' ' + userName);
+
+let usermoney=prompt ('¿Cúanto estas dispuesto a invertir en tu empresa?');
+validar=confirm ("Estas seguro");
+
+function saludar(){
+    document.write('Hola bienvenido'+ ' ' + userName);
+}
+
--- dinamico.js
+++ dinamico.js
@@ -20,3 +20,43 @@ if(color){
 }
 
 
+Funciones: un conjunto de instrucciones que trabajan de forma relacionada para un fin determinado. 
+
+Funciones: Admite parametros
+Puede retornar algún tipo de dato 
+Pueden ser incocados / llamadas desde fuera , pero tambien desde ReadableStreamDefaultController. 
+A parte de crear la función debo llamarla desde algun lugar 
+Si la función se llama bloque, para llmalar lo hago asi :'Bloque():
+
+Ejemplo
+function sumar(num1,num2){
+    let suma = num1 + num2;
+    return suma;
+    let x = sumar (8,12);
+
+    Function sumarDesdesPrompt()[{
+        let num1 = prompt("numero 1:");
+        let num2 = prompt ("Numero 2:");
+        Let suma = num1 + sum2;
+        alert (suma)
+    }
+    Let suma = num1 + num1;
+    alert (suma)
+
+
+    -----------------
+
+    document.write ("Bienvenido");
+    let userName = prompt("Cuales es tu nomre")
+    document.write ("Un gusto + userName");
+    Function obtener nombre () {
+
+    }
+
+    horaActual = parseINt{horaActual};
+
+
+    ---------
+    saludar
+    <script>
+        saludaPorHora ()
