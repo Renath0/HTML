45400bff0bb9d542e44d0fb461e4e11b914cf4d5
rennehuattamamani <rennehuatta@gmail.com>
2024-03-20 23:29:13 -0500

    Actualización 8

---
+++ Apuntes.js
@@ -0,0 +1,33 @@
+'use strict';
+alert("me gusta interrupir"); mensajes de SpeechRecognitionAlternative
+document.write("peru");
+console.log("Hola mundo");
+Funciones de output 
+
+Funciones para Input (recibor información)
+prompt("Cual tu nombre?");
+let username = prompt ("¿Cual es tu nombre");
+document.write("Bienvenido " + username);
+
+console.log("Welcome " + userName); | Importante es para saber como en algunos lugares de la programación. En producción en console.log cuando se ha llevado a un servidor.En
+
+confirm("Estas seguro?");
+let userConfirm = confirm ("Estas seguro");
+console.log(userConfirm);
+
+if (userConfirm) {
+    console.log("Acepto!!!")
+} else {
+    console.log("CANCELO!!!");
+}
+
+Enter pro skill Jueves 21 | de 7 a 10 pm
+ se realizara en Santa Cruz de 
+
+
+let confirm("Eres mayor de edad");
+let userName = prompt ("¿Cual es tu nombre");
+if (userConfirm) {
+    console.log ("Bienvenido chato" + userName)
+    
+}
--- Class08.js
+++ Class08.js
@@ -8,7 +8,7 @@ while (nombreEmpresa !=='ProgramadoresPe'){
 
 document.write('Bienvenido al sitio web de ProgramadoresPE!');
 
-let numero=prompt('Ingresa cuantas fotos quieres');
+let numero=prompt('Ingresa cuantas perritos quieres ver');
 for(let i=0;i<numero;i++){
     document.write('<img src="https://th.bing.com/th?id=OIP.cxN-GoALtVN6Cq0D5aHZNgHaEo&w=316&h=197&c=8&rs=1&qlt=30&o=6&pid=3.1&rm=2" with=100px>');
 }
