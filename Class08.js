f30f37387f23748f552e9f765f7c7fd70fee7c02
rennehuattamamani <rennehuatta@gmail.com>
2024-03-31 12:15:09 -0500

    CorreccionesClass7

--- Class08.js
+++ Class08.js
@@ -12,3 +12,5 @@ let numero=prompt('Ingresa cuantas perritos quieres ver');
 for(let i=0;i<numero;i++){
     document.write('<img src="https://th.bing.com/th?id=OIP.cxN-GoALtVN6Cq0D5aHZNgHaEo&w=316&h=197&c=8&rs=1&qlt=30&o=6&pid=3.1&rm=2" with=100px>');
 }
+
+for (let i=1;i<=24;i=i+1){}
--- class07.js
+++ class07.js
@@ -1,24 +1,20 @@
-function producto(){
-    let num1=3;
-    let num2=7;
-    producto=num1*num2;
-    return producto;
+function pedirNombre() {
+    return prompt("Ingresa tu nombre");
 }
 
-resultado=producto();
-console.log(resultado);
-
-document.write ('Hey ¿Qué tal?');
-userName=prompt('¿Cómo te llamas?:');
-document.write('Bienvenido'+ ' ' + userName);
-
-userempresa=prompt ('¿Qué estas buscado?:');
-ocument.write('Comprendido' + ' ' + userName);
-
-let usermoney=prompt ('¿Cúanto estas dispuesto a invertir en tu empresa?');
-validar=confirm ("Estas seguro");
-
-function saludar(){
-    document.write('Hola bienvenido'+ ' ' + userName);
+function saludar(usuarioNombre) {
+    document.write('Hola ' + usuarioNombre);
 }
 
+let edad = prompt("¿Cuál es tu edad?");
+edad = parseInt(edad);
+
+if (edad >= 18) {
+    alert("Bienvenido");
+} else {
+    alert("No se permite acceso a menores de edad. Nos vemos pronto");
+}
+
+function Ayuda() {
+    document.write('¿Cómo te podemos ayudar?');
+}
